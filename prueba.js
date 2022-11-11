1311a20f7a1afd87c1fd1169c55bb8847b39f694
AsmadyLopez <116740540+AsmadyLopez@users.noreply.github.com>
2022-11-11 10:35:10 -0500

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,8 @@
+console.log("BIENVENIDO A LA EVALUACIÓN DE JAVASCRIPT!!!")
+function mainFunction(){
+
+prinText=document.getElementById("Error").innerHTML
+alert(prinText)
+updateHeader=document.getElementById("Act").innerHTML="Cabecera Actualizada"
+
+}
